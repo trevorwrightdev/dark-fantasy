c7cfc439dcc77ac395843459cf522973c042a1c6
Trevor Wright <trevor.wright.710@gmail.com>
2023-10-14 19:03:05 -0600

    Initial commit.

---
+++ app/components/MusicButton.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { Howl } from 'howler'
+import { useState } from 'react'
+import SpeakerWave from './Icons/SpeakerWave'
+import SpeakerX from './Icons/SpeakerX'
+
+const sound = new Howl({
+    src: ['/music/yamaha.mp3'],
+    loop: true,
+})
+
+const MusicButton: React.FC = () => {
+
+    const [playing, setPlaying] = useState(false)
+
+    const togglePlayback = () => {
+        if (sound.playing()) {
+            sound.pause()
+            setPlaying(false)
+        } else {
+            sound.play()
+            setPlaying(true)
+        }
+    }   
+
+    return (
+        <main className='cursor-pointer flex flex-row justify-end p-4' onClick={togglePlayback}>
+            {playing ? <SpeakerWave /> : <SpeakerX />}
+        </main>
+    )
+}
+
+export default MusicButton
